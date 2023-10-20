f0e7f296585529d784b7a9e2252f390f9a2e18b6
James Muralles <james.muralles23@gmail.com>
2023-10-20 10:21:03 -0500

    changed navbar styling

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     const {palette} = useTheme();
     const [selectedPage, setSelectedPage] = useState("loginPage");
   return (
-    <FlexBetween mb="0.25rem" p=".5rem 0rem" color="black">
+    <FlexBetween mb="0.25rem" p=".5rem 0rem" color={palette.tertiary[500]}>
 
         {/* LEFT SIDE */}
         <FlexBetween gap=".75rem">
@@ -19,27 +19,28 @@ const NavBar = () => {
         </FlexBetween>
 
         {/* RIGHT SIDE */}
-        <FlexBetween gap="2rem">
+        <FlexBetween gap="2rem"  >
             {/* @ts-ignore */}
             <Box sx={{"&:hover": { color: palette.secondary[200]}}}>
                 <Link 
                 to="/"
                 onClick={()=> setSelectedPage("loginPage")}
                 style={{
-                    color: selectedPage === "loginPage" ? "inherit": palette.tertiary[500],
+                    color: selectedPage === "loginPage" ? "inherit": palette.grey[500],
                     textDecoration: "inherit"
                 }}
                 >
                 login</Link>
             </Box>
             {/* @ts-ignore */}
-            <Box sx={{"&:hover": { color: palette.primary[100]}}}>
+            <Box >
                 <Link 
                 to="/register"
-                onClick={()=> setSelectedPage("predictions")}
+                onClick={()=> setSelectedPage("registerPage")}
                 style={{
-                    color: selectedPage === "register" ? "inherit": palette.tertiary[500],
-                    textDecoration: "inherit"
+                    color: selectedPage === "registerPage" ? "inherit": palette.grey[500],
+                    textDecoration: "inherit",
+                    
                 }}
                 >
                 register</Link>
